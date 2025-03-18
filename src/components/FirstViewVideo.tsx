4002e1c956921962ff706eb0af404ad57cc49c66
FujieRyou <fujieryou83@gmail.com>
2025-03-18 09:54:12 +0900

    [mod]文言修正

--- src/components/FirstViewVideo.tsx
+++ src/components/FirstViewVideo.tsx
@@ -1,7 +1,7 @@
 export const FirstViewVideo = () => {
     return (
         <video className={"absolute top-0 h-screen w-screen object-cover object-center"}
-               src={"video/fvVideo.mp4"} autoPlay loop muted>
+               src={"video/fvVideo.mp4"} autoPlay loop muted playsInline>
         </video>
     )
 }
